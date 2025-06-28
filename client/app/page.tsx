141cdf25b8b773cc30ae9c5502045292f2a24ade
champNewbie <rattasat0012@gmail.com>
2025-06-28 23:03:44 +0700

    feat(client): Add login page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,103 +1,73 @@
-import Image from "next/image";
+'use client'
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa";
+import {AnimatePresence ,motion} from 'framer-motion'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="text-2xl font-bold flex justify-center items-center h-screen">
+      <AnimatePresence>
+      <Card className="flex justify-center w-[50%] border-[#27548A] border-4 shadow-[15px_5px_0_0px_#163f70]">
+        <CardHeader className="flex flex-col">
+          <CardTitle className="text-3xl">Welcome To Eduai</CardTitle>
+          <CardDescription>Courses Education Web and AI Assistant for study</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form action="">
+            <p className="text-lg">Email</p>
+            <Input
+              type="email"
+              placeholder="johnDoe@gmail.com"
+              className="border-none"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <p className="text-lg mt-2">Password</p>
+            <Input type="password" />
+          </form>
+          <div className="mt-2 flex justify-between items-center">
+              <Button className="
+              bg-[#A4B465]
+              text-white round-full 
+              cursor-pointer hover:bg-[#7c884c]
+              transition duration-400 shadow-[0_0_0_3px_#000000_inset] ">
+                Login
+              </Button>
+            <motion.a
+            initial={{scale : 1.0}}
+            whileHover={{scale : 1.1}}
+            href="#" className="text-sm hover:bg-[#ebd59d] duration-100 px-2 py-1 rounded-2xl">
+              Do you have a Account? 🤔
+            </motion.a>
+          </div>
+        </CardContent>
+        <CardFooter className='flex flex-col'>
+            <p className="text-sm mb-2">BUT!! , You can login with...</p>
+            <div className='flex flex-col'>
+              <Button className='bg-[#f2e5c4] flex flex-row items-center justify-center mb-2 
+              cursor-pointer hover:bg-[#ebd59d] duration-400'>
+                <FcGoogle />
+                <p className='text-sm ml-2'>Login With Google</p>
+              </Button>
+              <Button className='bg-[#f2e5c4] flex flex-row items-center justify-center cursor-pointer hover:bg-[#ebd59d] duration-400'>
+                <FaFacebook className='text-[#1778f2]'/>
+                <p className='text-sm ml-2'>Login With Facebook</p>
+              </Button>
+            </div>    
+        </CardFooter>
+      </Card>
+      </AnimatePresence>
     </div>
   );
-}
+};
+
+export default page;
